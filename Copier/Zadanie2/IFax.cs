a55dd703ab5a2928f1281cf9a50511741d685349
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-05-08 18:15:39 +0200

    Task2 Implemented

---
+++ Copier/Zadanie2/IFax.cs
@@ -0,0 +1,15 @@
+﻿
+using ver1;
+
+namespace Zadanie2
+{
+    public interface IFax
+    {
+        /// <summary>
+        /// Dokument jest wysyłany faxem, jeśli urządzenie włączone. W przeciwnym przypadku nic się nie wykonuje
+        /// </summary>
+        /// <param name="document">obiekt typu IDocument, różny od `null`</param>
+        void Fax(in IDocument document);
+    }
+
+}
---
+++ Copier/Zadanie2/MultifunctionalDevice.cs
@@ -0,0 +1,65 @@
+﻿
+using ver1;
+
+namespace Zadanie2
+{
+    public class MultifunctionalDevice : BaseDevice, IPrinter, IScanner, IFax
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+        public int FaxCounter { get; private set; } = 0;
+        public int Counter => PrintCounter + ScanCounter + FaxCounter;
+
+        public void Fax(in IDocument document)
+        {
+            if (GetState() == IDevice.State.off) return;
+            FaxCounter++;
+            Console.WriteLine($"{DateTime.Now} Fax: {document.GetFileName()}");
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (GetState() == IDevice.State.off) return;
+            PrintCounter++;
+
+            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            if (GetState() == IDevice.State.off)
+            {
+                document = null;
+                return;
+            }
+            ScanCounter++;
+
+            if (formatType == IDocument.FormatType.PDF)
+            {
+                document = new PDFDocument("xd.pdf");
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+            }
+            else if (formatType == IDocument.FormatType.JPG)
+            {
+                document = new ImageDocument("xd.jpg");
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+
+            }
+            else
+            {
+                document = new TextDocument("xd.txt");
+                Console.WriteLine($"{DateTime.Now} Scan: {document.GetFileName()}");
+            }
+
+        }
+
+        public void ScanAndPrint()
+        {
+            if (GetState() == IDevice.State.off) return;
+            IDocument document;
+
+            Scan(out document, IDocument.FormatType.JPG);
+            Print(document);
+        }
+    }
+}
